76d99ac0cf18edab5395e97968de07a84d081233
Lukas Sziele <lukassziele@aol.de>
2025-09-06 12:11:35 +0200

    Add .csproj to sln, Add C#12 feature examples
    .csproj is now correctly referenced in the solution file.
    Added most new features with simple examples in CSharp12 folder.

---
+++ CSharpInANutshell/CSharp12/CollectionExpressions.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpInANutshell.CSharp12;
+
+/// <summary>
+/// Collection expressions <c>(= ['a', 'b', 'c'])</c> allow easier collection creation.
+/// <para/>
+/// The same syntax works for different collection types, e.g., lists, sets, spans.
+/// <para/>
+/// They are target-typed, meaning you can omit the type where the compiler can infer it.
+/// <code>
+/// char[] charArray = ['a', 'b', 'c'];
+/// Foo(['a', 'b', 'c']);</code>
+/// </summary>
+public class CollectionExpressions
+{
+   private char[] _oldArray = { 'a', 'b', 'c' };
+   private char[] _newArray = ['a', 'b', 'c'];   // collection expression
+   
+   List<char> _oldList = new List<char> { 'a', 'b', 'c' };
+   private List<char> _somewhatOldList = new() { 'a', 'b', 'c' };
+   private List<char> _newList = ['a', 'b', 'c']; // collection expression
+}
